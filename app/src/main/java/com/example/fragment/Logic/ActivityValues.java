01e7689c45f2882782f9518108c28bc4e61189d2
Christopher Buchberger <c.buchbergercode@gmail.com>
2020-02-12 12:28:51 +0100

    Reuploaded ActivityValues

---
+++ app/src/main/java/com/example/fragment/Logic/ActivityValues.java
@@ -0,0 +1,14 @@
+package com.example.fragment.Logic;
+
+public class ActivityValues {
+
+   private String barcode;
+
+    public String getBarcode() {
+        return barcode;
+    }
+
+    public void setBarcode(String barcode) {
+        this.barcode = barcode;
+    }
+}
