c978dc6a4052551acf0abdbcd4c9084892433e15
Christopher Buchberger <c.buchbergercode@gmail.com>
2020-02-14 18:33:03 +0100

    Added Comments to Appdata

--- app/src/main/java/com/example/fragment/AppData/Logic/AppData.java
+++ app/src/main/java/com/example/fragment/AppData/Logic/AppData.java
@@ -24,7 +24,25 @@ public class AppData implements Serializable {
     private Settings settings;
     private ShoppingEntries shoppingEntries;
 
+    //region static Class
 
+    private static AppData instance = null;
+    public  static AppData getInstance(){
+        if(instance==null) {
+            instance = new AppData();
+        }
+        return instance;
+    }
+
+    //endregion
+
+
+    //the public Constructor is empty all data
+    // will be loaded after creation of the object.
+    public  AppData(){ }
+
+
+    //private Constructor for the save mechanism
     private AppData(Barcodes barcodes, Recipes recipes, Items items, Settings settings, ShoppingEntries shoppingEntries) {
         this.barcodes = barcodes;
         this.recipes = recipes;
@@ -33,9 +51,11 @@ public class AppData implements Serializable {
         this.shoppingEntries = shoppingEntries;
     }
 
+
+
+    //get the saved data from memory
     public void Init(){
         //Load all Lists, Settings and Barcodes from Memory
-
         //Retrieve the Data from the current Session
         ObjectInputStream input;
         try {
@@ -54,7 +74,6 @@ public class AppData implements Serializable {
     public void Save(){
         //Save all Lists, Settings and Barcodes from Memory
         AppData saveData = new AppData(barcodes,recipes,items,settings,shoppingEntries);
-
         //Save the current State to a file
         try {
             ObjectOutputStream output = new ObjectOutputStream(new FileOutputStream("test.txt"));
@@ -64,10 +83,12 @@ public class AppData implements Serializable {
 
     }
 
-    public void DeleteAppData(){
 
-        //Delete all Data
-        barcodes = new Barcodes();
+
+    //Create a new empty AppData Object and save it,
+    //to "delete" all memories.
+    public void DeleteAppData(){
+        barcodes = new Barcodes(new ArrayList<Barcode>());
         recipes = new Recipes();
         items = new Items();
         settings = new Settings();
@@ -76,15 +97,17 @@ public class AppData implements Serializable {
         Save();
     }
 
-
+    //get the barcode List
    public ArrayList<Barcode> getBarcodes(){
         return barcodes.getArray();
    }
 
+    //get the Recipe List
     public ArrayList<Recipe> getRecipes(){
         return recipes.getArray();
     }
 
+    //get the ShoppingEntry List
     public ArrayList<ShoppingEntry> getShoppingEntries(){
         return shoppingEntries.getArray();
     }
