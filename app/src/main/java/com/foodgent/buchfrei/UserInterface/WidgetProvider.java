fb4e5706450c38ee5467747f1a842865c317de2d
Robert Freiseisen <r.superfreisi@gmail.com>
2020-05-08 08:16:41 +0200

    Add Widget Provider!!

---
+++ app/src/main/java/com/foodgent/buchfrei/UserInterface/WidgetProvider.java
@@ -0,0 +1,24 @@
+package com.foodgent.buchfrei.UserInterface;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import com.example.fragment.R;
+
+public class WidgetProvider extends AppWidgetProvider {
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        for (int appWidgetId : appWidgetIds){
+            Intent intent = new Intent(context, MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.example_widget);
+
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
+}
