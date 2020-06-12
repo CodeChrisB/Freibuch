5783a72ef69673c04b56a8ed47a6fbcd13a6443f
Christopher Buchberger <c.buchbergercode@gmail.com>
2020-06-12 09:45:50 +0200

    Final Source Code For Syp
    Signed-off-by: Christopher Buchberger <c.buchbergercode@gmail.com>

--- app/src/main/java/com/foodgent/buchfrei/Entity/ItemListAdapter.java
+++ app/src/main/java/com/foodgent/buchfrei/Entity/ItemListAdapter.java
@@ -5,17 +5,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.fragment.R;
 import com.foodgent.buchfrei.AppData.Entities.Item;
 import com.foodgent.buchfrei.AppData.Logic.AppData;
-import com.foodgent.buchfrei.UserInterface.MainActivity;
 
 import java.util.ArrayList;
 
@@ -29,7 +26,7 @@ public class ItemListAdapter extends RecyclerView.Adapter<ItemListAdapter.ItemLi
     public ItemListAdapter.ItemListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_itemconstraint, parent, false);
         final ItemListAdapter.ItemListViewHolder  vHolder= new ItemListAdapter.ItemListViewHolder(v);
-        v.setOnClickListener(new View.OnClickListener() {
+        /*v.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 current = items.get(vHolder.getAdapterPosition());
@@ -45,7 +42,7 @@ public class ItemListAdapter extends RecyclerView.Adapter<ItemListAdapter.ItemLi
                 helpDialog.setView(alertItemView);
                 helpDialog.show();
             }
-        });
+        });*/
 
         return vHolder;
     }
